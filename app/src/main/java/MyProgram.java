3af14284647e2776f8a87d1dc91d95b49238adac
Vladimir Ilyin <oivt@ya.ru>
2017-03-23 23:12:16 +0300

    System.setIn(), System.setOut()

--- app/src/main/java/MyProgram.java
+++ app/src/main/java/MyProgram.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class MyProgram
 {
-
+    static Scanner in = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		System.out.println("Enter two numbers");
+
+        System.out.println("Enter two numbers");
 		int x = in.nextInt(), y = in.nextInt();
 		int sum = x + y;
 		System.out.println("Sum of them is " + sum);
