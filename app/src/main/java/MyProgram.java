75bd5d5d930813b01a50983161921be5d912ed1f
Vladimir Ilyin <oivt@ya.ru>
2017-03-25 19:42:44 +0300

    delete colors.xml

--- app/src/main/java/MyProgram.java
+++ app/src/main/java/MyProgram.java
@@ -1,15 +1,8 @@
-import java.util.Scanner;
-
 public class MyProgram
 {
-    static Scanner in = new Scanner(System.in);
 
 	public static void main(String[] args) {
-
-        System.out.println("Enter two numbers");
-		int x = in.nextInt(), y = in.nextInt();
-		int sum = x + y;
-		System.out.println("Sum of them is " + sum);
+		System.out.println("Hello, world!");
 	}
 }
  
