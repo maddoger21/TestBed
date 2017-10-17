b8d1323a778b9498b92b727ce0a7cbc7c3b9fbee
vv73 <oivt@ya.ru>
2017-10-17 23:30:05 +0300

    Update MyProgram.java

--- app/src/main/java/MyProgram.java
+++ app/src/main/java/MyProgram.java
@@ -1,7 +1,5 @@
-public class MyProgram
-{
-
-	public static void main(String[] args) {
+public class MyProgram{
+	public static void main(String[] args){
 		System.out.println("Hello, world!");
 	}
 }
